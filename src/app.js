1c60dde0efdfdd52c74a242728613cc8219de0b7
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-15 20:25:34 +0545

    Random

--- src/app.js
+++ src/app.js
@@ -1,28 +1,23 @@
-import React from "react";
-import { useEffect } from "react";
-
-// key: 22e1362
-const API_URL = "http://www.omdbapi.com?apikey=22e1362"
-
+import React, { useEffect } from "react";
+import "./app.css";
+// API_KEY = 22e1362
+const API_URL = "http://www.omdbapi.com?apikey=22e1362";
 
 // Main functional component
-const App = () =>
-{
-    // Async means it takes some time to fetch these movies
-    const searchMovies = async(title) =>{
-        const response = await fetch(`${API_URL} & s=${title}`);
-        const data = await response.json();
+const App = () => {
+  // Async means it takes some time to fetch these movies
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-        console.log(data);
-    } 
-    useEffect(()=>{
-        searchMovies('Spiderman');
-    }, []); //Empty dependency array helps to call this function only at the reload or start
-    return(
-        <h1>
-            App
-        </h1>
-    )
-}
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []); // Empty dependency array helps to call this function only at the reload or start
+
+  return <h1>App</h1>;
+};
 
 export default App;
