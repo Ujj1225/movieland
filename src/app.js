654fd235b9f180e1de42c7f336aa61683562f998
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-16 03:49:06 +0545

    Search bar ready

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./app.css";
+import searchIcon from "./search.svg";
 // API_KEY = 22e1362
 const API_URL = "http://www.omdbapi.com?apikey=22e1362";
 
@@ -17,7 +18,23 @@ const App = () => {
     searchMovies("Spiderman");
   }, []); // Empty dependency array helps to call this function only at the reload or start
 
-  return <h1>App</h1>;
+  return (
+    <div classname="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Fuck Man"
+          onChange={() => {}}
+        />
+        <img
+        src = {searchIcon}
+        alt="Search"
+        onClick={()=>{}}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
